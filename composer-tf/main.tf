98914756e98098e8409d66c922e47e128976e903
Patrick Haggerty <hagg@haggconsulting.com>
2024-08-11 18:22:32 +0000

    Initial env. create TF scripts

---
+++ composer-tf/main.tf
@@ -0,0 +1,37 @@
+// Enable the Cloud Composer API
+resource "google_project_service" "composer_api" {
+  project = var.project_id
+  service = "composer.googleapis.com"
+  // Disabling the API while environments are running
+  // can seriously break stuff. Remove the below once
+  // all environments are deleted.
+  disable_on_destroy = false
+}
+
+// Create the custom service account for the nodes
+module "composer_service_account" {
+  source  = "terraform-google-modules/service-accounts/google"
+  version = "~> 4.0"
+
+  project_id = var.project_id
+  names      = ["composer-account"]
+  // Add roles as needed
+  project_roles = ["${var.project_id}=>roles/composer.worker"]
+  display_name  = "Composer Account"
+}
+
+// Create the Composer instance
+resource "google_composer_environment" "lab_environment" {
+  name   = "lab-environment"
+  project = var.project_id
+  region = var.default_region
+
+  config {
+    software_config {
+      image_version = "composer-3-airflow-2.7.3-build.11"
+    }
+    node_config {
+      service_account = module.composer_service_account.email
+    }
+  }
+}
---
+++ composer-tf/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id     = "qwiklabs-gcp-03-f2a704a6d265"
+default_region = "us-central1"
