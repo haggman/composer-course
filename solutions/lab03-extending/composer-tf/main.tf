46d9cf59c62c342b3edded39fc1e2e914d57cc2e
Patrick Haggerty <hagg@haggconsulting.com>
2024-08-30 03:51:41 +0000

    lab 3 initial push

---
+++ solutions/lab03-extending/composer-tf/main.tf
@@ -0,0 +1,44 @@
+// Enable the Cloud Composer API
+resource "google_project_service" "composer_api" {
+  project = var.project_id
+  service = "composer.googleapis.com"
+  // Disabling the API while environments are running
+  // can seriously break stuff. Remove the below once
+  // all environments are deleted.
+  disable_on_destroy = false
+}
+
+// Create the custom service account for the nodes
+resource "google_service_account" "composer_account" {
+  account_id   = "composer-account"
+  display_name = "Custom SA for Cloud Composer Nodes"
+}
+// Bind the new service account to its roles
+resource "google_project_iam_member" "composer_account_bindings" {
+  for_each = toset([
+    "roles/composer.worker",
+    "roles/storage.admin", // So Airflow can access GCS
+  ])
+
+  project  = var.project_id
+  member   = "serviceAccount:${google_service_account.composer_account.email}"
+  role     = each.value
+}
+
+// Create the Composer instance
+resource "google_composer_environment" "lab_environment" {
+  name    = "lab-environment"
+  project = var.project_id
+  region  = var.default_region
+
+  config {
+    software_config {
+      image_version = "composer-3-airflow-2"
+    }
+    node_config {
+      service_account = google_service_account.composer_account.email
+    }
+
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+  } // ENVIRONMENT_SIZE_MEDIUM, ENVIRONMENT_SIZE_LARGE
+}
---
+++ solutions/lab03-extending/workflow/data_processing_workflow.py
@@ -0,0 +1,43 @@
+import os
+
+from airflow import DAG
+from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+# Define the DAG
+dag = DAG(
+    'data_processing_workflow',
+    catchup = False,
+    start_date = days_ago(0),
+    schedule_interval=None,
+)
+
+# Get the directory of the current DAG file
+dag_folder = os.path.dirname(__file__)
+
+# Get the project ID from the variable
+project_id = Variable.get("project_id")
+
+# Create the GCS bucket if it doesn't exist
+create_bucket = GCSCreateBucketOperator(
+    task_id='create_bucket',
+    bucket_name=project_id,
+    project_id=project_id,
+    location='us-central1',
+    storage_class='STANDARD',
+    labels={'env': 'dev', 'team': 'airflow'},
+    dag=dag
+)
+
+# Upload the file to GCS
+upload_sample_data = LocalFilesystemToGCSOperator(
+    task_id='upload_to_gcs',
+    bucket=project_id,
+    dst='sample_data/events.json',
+    src = os.path.join(dag_folder, 'sample_data/events.json'),
+    dag=dag,
+)
+
+create_bucket >> upload_sample_data
